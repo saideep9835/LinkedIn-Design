9b1b0d6735b740dad21ad2e2e9dc3ea6211d681e
Saideep9835 <tsaideep98@gmail.com>
2024-09-25 13:03:00 -0500

    used UserDefault for email and password

--- LinkedIn Design/ViewController.swift
+++ LinkedIn Design/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var emailTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+       
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func signInButton(_ sender: UIButton) {
+        
+        
+        let userDefault = UserDefaults.standard
+        userDefault.set(emailTF.text, forKey: "email")
+        userDefault.set(passwordTF.text, forKey: "password")
+        if let email = userDefault.string(forKey: "email"), let password = userDefault.string(forKey: "password") {
+            self.email.text = email
+            self.passwordLabel.text = password
+            print("Email: \(email)")
+            print("Password: \(password)")
+        }
+    }
+    
 }
 
